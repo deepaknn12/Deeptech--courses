05bbd04b71a66350ead311e8af84fe1d6cb9958a
Deepak Kushwaha <145982737+deepaknn12@users.noreply.github.com>
2025-05-31 20:31:55 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = 4001;
-const MongoDBURI = "mongodb+srv://kumardeepu841231:kCVNMsfYi42hZoQM@cluster450.ybtvdfy.mongodb.net/Bookstore?retryWrites=true&w=majority&appName=Cluster450";
+const PORT = process.env.PORT || 4002;
+const MongoDBURI = process.env.MONGODB_URI;
 
-// ⚡ Updated: removed deprecated options
 mongoose.connect(MongoDBURI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("Error: ", error));
